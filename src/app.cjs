476215431223984e17cb5363e4f55d5d0d505a6d
vcat <1991416497@qq.com>
2024-03-10 20:01:44 +0800

    对项目结构稍作修改

---
+++ src/app.cjs
@@ -0,0 +1,13 @@
+// app.cjs
+
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
