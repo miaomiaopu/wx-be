58b4447d8bbfb9615106a568ab002ad586b4b2df
vcat <1991416497@qq.com>
2024-03-11 14:40:22 +0800

    完善项目结构，修改 package.json 启动脚本中，start为serve，添加 .git 忽略项，生成项目结构树，删除测试用中间件

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 基于Node.js的知识卡片分享平台的后端框架
 
 使用 Express 后端框架开发
+```
+wx-be
+├─ .gitignore
+├─ LICENSE
+├─ package-lock.json
+├─ package.json
+├─ README.md
+└─ src
+   ├─ app.cjs
+   ├─ middlewares
+   └─ models
+```
--- src/app.cjs
+++ src/app.cjs
@@ -4,10 +4,6 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
