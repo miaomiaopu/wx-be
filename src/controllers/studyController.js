7170e4cd3c3868ee0b7f262f6283fc099290648d
vcat <1991416497@qq.com>
2024-04-29 18:08:19 +0800

    complete study card

--- src/controllers/studyController.js
+++ src/controllers/studyController.js
@@ -1,5 +1,12 @@
 import { Op } from "sequelize";
-import { CardStudyTime, ThemeCardConnection } from "../models/index.js";
+import sequelize from "../configs/database.js";
+import {
+  Card,
+  CardPicture,
+  CardStudyTime,
+  Data,
+  ThemeCardConnection,
+} from "../models/index.js";
 import logger from "../configs/logger.js";
 import redisPool from "../configs/redis.js";
 
@@ -289,13 +296,273 @@ const cancelSelectCard = async (req, res) => {
 };
 
 const getStudyCards = async (req, res) => {
-  // 选择算法，阶段为 0 的 最后选
+  // 选择算法，阶段为 0 的 最后选，阶段为 3 的不选
   // 优先选择距离需要复习时间较长的卡片
   // 例: 0-1，1-2，2-3，今天天数为 4，选择 2 号
-  // 每次选择10个进行一轮复习
+  // 每次选择10个进行一轮复习，不足十个的选择所有
+  try {
+    logger.info("/api/getStudyCards");
+    const { third_session } = req.query;
+
+    let openid = null;
+    await redisPool.get(third_session, (err, result) => {
+      if (err) {
+        logger.error(`Redis error: ${err}`);
+      } else {
+        openid = result;
+      }
+    });
+
+    if (!openid) {
+      res.status(404).json({ message: "Third session key not found" });
+    } else {
+      let studyTimes = [];
+      const now = new Date();
+      // phase = 0 ; phase = 1, 当前时间 > last_study_time + 1 天 时; phase = 2, 当前时间 > last_study_time + 3;
+      // 并且根据 当前时间 - (last_study_time + ?) 的 插值进行排序, 越大越早。选最多10个
+      await CardStudyTime.findAll({
+        where: {
+          openid: openid,
+          // 使用 Sequelize 的符号运算符来进行条件判断
+          [Op.or]: [
+            // phase = 0 或者 phase = 1 且当前时间小于 last_study_time + 1 天
+            {
+              [Op.or]: [
+                { phase: 0 },
+                {
+                  phase: 1,
+                  last_study_time: {
+                    [Op.lte]: new Date(now.getTime() - 24 * 60 * 60 * 1000), // 当前时间减去 1 天的毫秒数
+                  },
+                },
+              ],
+            },
+            // phase = 2 且当前时间小于 last_study_time + 3 天
+            {
+              phase: 2,
+              last_study_time: {
+                [Op.lte]: new Date(now.getTime() - 3 * 24 * 60 * 60 * 1000), // 当前时间减去 3 天的毫秒数
+              },
+            },
+          ],
+        },
+        order: [
+          ["phase", "DESC"], // 先按照 phase 排序
+          ["last_study_time", "ASC"], // 如果 phase 相同，则按照 last_study_time 降序排序
+        ],
+        limit: 10,
+      }).then((res) => {
+        // studyTimes = [{card_id, phase}, {}]
+        res.forEach((item) => {
+          studyTimes.push({
+            card_id: item.card_id,
+            phase: item.phase,
+            s_phase: 0,
+          });
+        });
+      });
+
+      if (studyTimes.length == 0) {
+        res.status(202).json({
+          message: "Study card is null",
+        });
+      } else {
+        res.status(200).json({
+          message: "Get study cards successful",
+          studyTimes,
+        });
+      }
+    }
+  } catch (error) {
+    logger.error(`Error Get study cards: ${error}`);
+    res.status(500).json({ message: "Failed to Get study cards" });
+  }
 };
 
-const completeStudyCard = async (req, res) => {};
+const getStudyCard = async (req, res) => {
+  try {
+    logger.info("/api/getCard");
+
+    const third_session = req.query.third_session;
+    const card_id = req.query.card_id;
+
+    let openid = null;
+    await redisPool.get(third_session, (err, result) => {
+      if (err) {
+        logger.error(`Redis error: ${err}`);
+      } else {
+        openid = result;
+      }
+    });
+
+    if (!openid) {
+      res.status(404).json({ message: "Third session key not found" });
+    } else {
+      let card_title = "",
+        str1 = "",
+        str2 = "",
+        str3 = "",
+        str4 = "",
+        image1 = "",
+        image2 = "",
+        image3 = "";
+
+      // 获取card_content
+      let content = "";
+      await Card.findOne({ where: { card_id: card_id } }).then((result) => {
+        card_title = result.card_title;
+        content = result.card_content;
+      });
+
+      const regex = /<i>\d<i>/g;
+
+      const str = content.split(regex);
+
+      for (let index = 0; index < str.length; index++) {
+        const element = str[index];
+        if (index == 0) {
+          str1 = element;
+        } else if (index == 1) {
+          str2 = element;
+        } else if (index == 2) {
+          str3 = element;
+        } else if (index == 3) {
+          str4 = element;
+        }
+      }
+
+      let images = [];
+      await CardPicture.findAll({
+        where: { card_id: card_id },
+        order: [["card_picture_id", "ASC"]],
+      }).then((result) => {
+        images = result.map((res) => res.picture);
+      });
+
+      for (let index = 0; index < images.length; index++) {
+        const element = images[index];
+        if (index == 0) {
+          image1 = element;
+        } else if (index == 1) {
+          image2 = element;
+        } else if (index == 2) {
+          image3 = element;
+        }
+      }
+
+      res.status(200).json({
+        message: "get card successful",
+        card_title: card_title,
+        str1: str1,
+        str2: str2,
+        str3: str3,
+        str4: str4,
+        image1: image1,
+        image2: image2,
+        image3: image3,
+      });
+    }
+  } catch (error) {
+    logger.error(`Error get card: ${error}`);
+    res.status(500).json({ message: "Fail to get card" });
+  }
+};
+
+const completeStudyCard = async (req, res) => {
+  try {
+    logger.info("/api/completeStudyCard");
+    const { third_session, card_id } = req.body;
+
+    let openid = null;
+    await redisPool.get(third_session, (err, result) => {
+      if (err) {
+        logger.error(`Redis error: ${err}`);
+      } else {
+        openid = result;
+      }
+    });
+
+    if (!openid) {
+      res.status(404).json({ message: "Third session key not found" });
+    } else {
+      // 先查询出需要更新的数据
+      const cardStudyTime = await CardStudyTime.findOne({
+        where: { card_id: card_id, openid: openid },
+      });
+
+      // 如果找到了对应的记录，则更新
+      if (cardStudyTime) {
+        if (cardStudyTime.phase == 0) {
+          await Data.update(
+            { today_study: sequelize.literal("today_study + 1") },
+            { where: { openid: openid } }
+          );
+        } else if (cardStudyTime.phase == 1) {
+          await Data.update(
+            { today_review: sequelize.literal("today_review + 1") },
+            { where: { openid: openid } }
+          );
+        } else if (cardStudyTime.phase == 2) {
+          await Data.update(
+            {
+              today_review: sequelize.literal("today_review + 1"),
+              total_learn: sequelize.literal("total_learn + 1"),
+            },
+            { where: { openid: openid } }
+          );
+        }
+
+        const newPhase = cardStudyTime.phase + 1; // 计算新的 phase
+        await CardStudyTime.update(
+          { phase: newPhase },
+          { where: { card_id: card_id, openid: openid } }
+        );
+      }
+
+      res.status(200).json({
+        message: "Complete Study Card successful",
+      });
+    }
+  } catch (error) {
+    logger.error(`Error Complete Study Card: ${error}`);
+    res.status(500).json({ message: "Failed to Complete Study Card" });
+  }
+};
+
+const studyTime = async (req, res) => {
+  try {
+    logger.info("/api/studyTime");
+    const { third_session, duration } = req.body;
+
+    let openid = null;
+    await redisPool.get(third_session, (err, result) => {
+      if (err) {
+        logger.error(`Redis error: ${err}`);
+      } else {
+        openid = result;
+      }
+    });
+
+    if (!openid) {
+      res.status(404).json({ message: "Third session key not found" });
+    } else {
+      await Data.update(
+        {
+          today_duration: sequelize.literal(`today_duration + ${duration}`),
+          total_duration: sequelize.literal(`total_duration + ${duration}`),
+        },
+        { where: { openid: openid } }
+      );
+
+      res.status(200).json({
+        message: "Study Time successful",
+      });
+    }
+  } catch (error) {
+    logger.error(`Error Study Time: ${error}`);
+    res.status(500).json({ message: "Failed to Study Time" });
+  }
+};
 
 export {
   getIsAllStudy,
@@ -305,5 +572,7 @@ export {
   cancelSelectCards,
   cancelSelectCard,
   getStudyCards,
+  getStudyCard,
   completeStudyCard,
+  studyTime,
 };
--- src/routes/studyRoutes.js
+++ src/routes/studyRoutes.js
@@ -7,18 +7,22 @@ import {
   cancelSelectCards,
   cancelSelectCard,
   getStudyCards,
+  getStudyCard,
   completeStudyCard,
+  studyTime
 } from "../controllers/studyController.js";
 
 const studyRouter = express.Router();
 
 studyRouter.get("/getIsStudy", getIsStudy);
-studyRouter.get("/getIsAllStudy", getIsAllStudy)
+studyRouter.get("/getIsAllStudy", getIsAllStudy);
 studyRouter.post("/selectCard", selectCard);
 studyRouter.post("/selectCards", selectCards);
 studyRouter.post("/cancelSelectCards", cancelSelectCards);
 studyRouter.post("/cancelSelectCard", cancelSelectCard);
 studyRouter.get("/getStudyCards", getStudyCards);
+studyRouter.get("/getStudyCard", getStudyCard);
 studyRouter.post("/completeStudyCard", completeStudyCard);
+studyRouter.post("/studyTime", studyTime)
 
 export default studyRouter;
